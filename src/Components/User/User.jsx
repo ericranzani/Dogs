b5c3afa2e9afce69672dda540bd037915765f1e9
Éric Ranzani <eric_ranzani@yahoo.com.br>
2025-08-12 12:47:46 -0300

    desenvolvido o scroll infinito que vai limitar a pagina com a quantidade de imagens que tem

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -4,13 +4,16 @@ import UserStats from './UserStats';
 import Feed from '../Feed/Feed';
 import UserPhotoPost from './UserPhotoPost'
 import { Route, Routes } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
 
 const User = () => {
+  const {data} = React.useContext(UserContext);
+
   return (
     <section className='container'>
       <UserHeader />
       <Routes>
-        <Route path='/' element={<Feed />} />
+        <Route path='/' element={<Feed user={data.id} />} />
         <Route path='postar' element={<UserPhotoPost />} />
         <Route path='estatisticas' element={<UserStats />} />
       </Routes>
