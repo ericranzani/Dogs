b9f69be202b29c9d8fec2174ce5f810804ae7bf8
Éric Ranzani <eric_ranzani@yahoo.com.br>
2025-08-13 11:36:08 -0300

    desenvolvido a pagina quando clica em uma foto especifica, e refatoração de alguns arquivos para correção de alguns bugs

--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import styles from './FeedPhotosItem.module.css';
 import Image from '../Helper/Image';
 
-
-const FeedPhotosItem = ({photo, setModalPhoto}) => {
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   function handleClick() {
     setModalPhoto(photo);
   }
-
   return (
     <li className={styles.photo} onClick={handleClick}>
       <Image src={photo.src} alt={photo.title} />
--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -1,43 +1,44 @@
 import React from 'react';
 
 const types = {
-    email: {
-        regex: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        massage: 'Preeencha um e-mail valido',
-    },
-    password: {
-        regex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/,
-        massage: 'Preencher uma senha valida com pelo menos uma letra maiuscula, um digito númerico e um caracter especial',
-    },
-    number: {
-        regex: /^\d+$/,
-        message: 'Utilize apensa números',
-    }
-}
+  email: {
+    regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: 'Preencha um email válido',
+  },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.',
+  },
+  number: {
+    regex: /^\d+$/,
+    message: 'Utilize números apenas.',
+  },
+};
 
 const useForm = (type) => {
-    const[value, setValue] = React.useState('');
-    const[error, setError] = React.useState(null);
+  const [value, setValue] = React.useState('');
+  const [error, setError] = React.useState(null);
 
-    function validate(value) {
-        if(type === false) return true;
-        if(value.length === 0) {
-            setError("Preencha um valor.");
-            return false;
-        } else if (types[type] && !types[type].regex.test(value)) {
-            setError(types[type].massage);
-            return false;
-        } else {
-            setError(null);
-            return true;
-        }
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError('Preencha um valor.');
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
     }
+  }
 
-    function onChange({target}) {
-        if(error) validate(target.value);
-        setValue(target.value);
-    }
- 
+  function onChange({ target }) {
+    if (error) validate(target.value);
+    setValue(target.value);
+  }
+
   return {
     value,
     setValue,
