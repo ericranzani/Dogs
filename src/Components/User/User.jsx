46878257fe9a05d62201c9a6e443a6149c0adb9a
Éric Ranzani <eric_ranzani@yahoo.com.br>
2025-08-13 12:59:32 -0300

    criado e adicionado o Head das paginas

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -6,12 +6,14 @@ import UserPhotoPost from './UserPhotoPost'
 import { Route, Routes } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
 import NotFound from '../NotFound';
+import Head from '../Helper/Head';
 
 const User = () => {
   const {data} = React.useContext(UserContext);
 
   return (
     <section className='container'>
+      <Head title="Minha Conta" />
       <UserHeader />
       <Routes>
         <Route path='/' element={<Feed user={data.id} />} />
